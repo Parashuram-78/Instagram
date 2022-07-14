04952531a76d7acd14130d198272fe22bba42674
Ankur Shukla <itzankurhere@gmail.com>
2022-07-14 12:17:54 +0530

    Update server.js

--- instagram-backend-node/server.js
+++ instagram-backend-node/server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 dotenv.config({ path: "./config.env" });
 const routes = require("./routes");
 const db_url = process.env.DB_URL;
-
+const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
